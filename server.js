d3b550c287cb6fa599547de0b6b957e90b340dbf
aapelix <mcaapelix@gmail.com>
2025-01-04 19:21:20 +0200

    init

---
+++ server.js
@@ -0,0 +1,45 @@
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+
+const httpServer = createServer();
+const io = new Server(httpServer, { 
+    cors: {
+        origin: "*",
+    }
+});
+
+let players = {};
+const colors = ['red', 'blue', 'green', 'yellow', 'purple', 'orange', 'pink', 'brown', 'black', 'white'];
+
+
+
+io.on('connection', (socket) => {
+    console.log('A player connected: ', socket.id);
+
+    const randomColor = colors[Math.floor(Math.random() * colors.length)];
+
+    players[socket.id] = { 
+        pos: [250, 250], angle: 0, velocity: 0, color: randomColor};
+
+    // Send the current players' state to the new player
+    socket.emit('init', players);
+
+    // Listen for movement updates from this player
+    socket.on('move', (data) => {
+        if (players[socket.id]) {
+            players[socket.id] = { ...players[socket.id], ...data };
+        }
+
+        // Broadcast updated player data to all clients
+        io.emit('update', players);
+    });
+
+    // Handle player disconnect
+    socket.on('disconnect', () => {
+        console.log('A player disconnected: ', socket.id);
+        delete players[socket.id];
+        io.emit('update', players); // Broadcast updated player list
+    });
+});
+
+httpServer.listen(3000);
